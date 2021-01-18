b657871e80f9ca87d25b0e5bd5f5e7ae48e1b457
jtimonen <juho.timonen@iki.fi>
2021-01-18 21:08:39 +0200

    edit bb tutorial

--- tutorials/knit_tutorials.sh
+++ tutorials/knit_tutorials.sh
@@ -1,4 +1,10 @@
+date
 Rscript -e 'library(rmarkdown); rmarkdown::render("basic/basic.Rmd", "html_document")'
+date
 Rscript -e 'library(rmarkdown); rmarkdown::render("heter/heter.Rmd", "html_document")'
+date
 Rscript -e 'library(rmarkdown); rmarkdown::render("uncrt/uncrt.Rmd", "html_document")'
+date
 Rscript -e 'library(rmarkdown); rmarkdown::render("bb/bb.Rmd", "html_document")'
+date
+
