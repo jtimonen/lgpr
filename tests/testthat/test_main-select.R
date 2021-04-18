18a06787cc32deaa663c20d1aee0b776536f3498
jtimonen <juho.timonen@iki.fi>
2021-04-18 17:47:25 +0300

    add tests for select()

---
+++ tests/testthat/test_main-select.R
@@ -0,0 +1,60 @@
+library(lgpr)
+source("helpers/SW.R")
+# source("tests/testthat/helpers/SW.R")
+
+# -------------------------------------------------------------------------
+N_ITER <- 18 # should be even
+N_CHAINS <- 1
+
+context("Covariate selection")
+
+test_that("selection can be done when sample_f = FALSE", {
+  SW({
+    fit <- example_fit(iter = N_ITER, chains = N_CHAINS)
+  })
+  sel_before <- select(fit)
+  expect_equal(length(sel_before$Component), 5)
+
+  # After clearing postproc info
+  fit <- clear_postproc(fit)
+  expect_output(
+    {
+      expect_message(
+        {
+          select(fit, verbose = TRUE)
+        },
+        "No existing postprocessing information stored"
+      )
+    },
+    "Creating kernel matrices"
+  )
+  sel_after <- select(fit, verbose = FALSE)
+  expect_equal(sel_before, sel_after)
+})
+
+test_that("selection can be done when sample_f = TRUE", {
+  SW({
+    fit <- example_fit(iter = N_ITER, chains = N_CHAINS, likelihood = "binomial")
+  })
+  sel_before <- select(fit, threshold = 0.3)
+  expect_equal(length(sel_before$Component), 5)
+
+  # After clearing postproc info
+  fit <- clear_postproc(fit)
+  sel_after <- select(fit, verbose = FALSE, threshold = 0.3)
+  expect_equal(sel_before, sel_after)
+})
+
+test_that("other select() functions can be used", {
+  SW({
+    fit <- example_fit(iter = N_ITER, chains = N_CHAINS)
+  })
+  sel <- select_freq(fit)
+  expect_equal(length(sel$Component), 5)
+
+  sel <- select.integrate(fit, verbose = FALSE)
+  expect_equal(dim(sel$selected), c(101, 5))
+
+  sel <- select_freq.integrate(fit, verbose = FALSE)
+  expect_equal(dim(sel$freq), c(101, 5))
+})
