affc375dc7e0f96e34ebafc55ba5f1ef65bbfa42
jtimonen <juho.timonen@iki.fi>
2021-06-10 00:37:33 +0300

    edit tutorials knitting script

--- tutorials/knit_tutorials.sh
+++ tutorials/knit_tutorials.sh
@@ -1,10 +1,28 @@
+#!/bin/bash
+HL1='\033[0;32m'
+NC='\033[0m' # No Color
+
+printf "\n${HL1} (1) -----------------------------------------------------------------\n"
 date
+printf "${NC}\n"
 Rscript -e 'library(rmarkdown); rmarkdown::render("basic/basic.Rmd", "html_document")'
+
+printf "\n${HL1} (2) -----------------------------------------------------------------\n"
 date
+printf "${NC}\n"
 Rscript -e 'library(rmarkdown); rmarkdown::render("heter/heter.Rmd", "html_document")'
+
+printf "\n${HL1} (3) -----------------------------------------------------------------\n"
 date
+printf "${NC}\n"
 Rscript -e 'library(rmarkdown); rmarkdown::render("uncrt/uncrt.Rmd", "html_document")'
+
+printf "\n${HL1} (4) -----------------------------------------------------------------\n"
 date
+printf "${NC}\n"
 Rscript -e 'library(rmarkdown); rmarkdown::render("bb/bb.Rmd", "html_document")'
 date
 
+printf "\n${HL1} (FINISH) ------------------------------------------------------------\n"
+date
+printf "${NC}\n"
